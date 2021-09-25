9d022fada6f7785e0bf2614bc1461ea27ca8ec81
dchicchon <danielchicchon@gmail.com>
2021-09-25 17:12:32 -0400

    update tsconfig.json

---
+++ build/schema/resolvers/location/location.test.js
@@ -0,0 +1,133 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const createTestServer_1 = require("../../testUtils/createTestServer");
+const LocationQueries = __importStar(require("../../testUtils/locationQueries"));
+const OrganizationQueries = __importStar(require("../../testUtils/organizationQueries"));
+describe('resolvers', () => {
+    let server;
+    let organizationId;
+    let locationId;
+    beforeAll(async () => {
+        var _a, _b;
+        server = await (0, createTestServer_1.createTestServer)();
+        const createOrganizationResult = await server.executeOperation({
+            query: OrganizationQueries.CREATE_ORGANIZATION,
+            variables: { name: "Twitter" }
+        });
+        organizationId = (_a = createOrganizationResult.data) === null || _a === void 0 ? void 0 : _a.createOrganization.id;
+        const createLocationResult = await server.executeOperation({
+            query: LocationQueries.CREATE_LOCATION,
+            variables: {
+                name: "Twitter SF Headquarters",
+                address: "1355 Market St #900, San Francisco, CA 94103",
+                organizationId: organizationId,
+            }
+        });
+        locationId = (_b = createLocationResult.data) === null || _b === void 0 ? void 0 : _b.createLocation.id;
+    });
+    afterAll(async () => {
+        var _a;
+        const findResult = await server.executeOperation({
+            query: OrganizationQueries.GET_ALL_ORGANIZATIONS
+        });
+        for (const org of (_a = findResult.data) === null || _a === void 0 ? void 0 : _a.allOrganizations) {
+            const organizationId = org.id;
+            const deleteResult = await server.executeOperation({
+                query: OrganizationQueries.DELETE_ORGANIZATION,
+                variables: { id: organizationId }
+            });
+        }
+    });
+    it("Creates a Location", async () => {
+        var _a;
+        expect.assertions(2);
+        const createResult = await server.executeOperation({
+            query: LocationQueries.CREATE_LOCATION,
+            variables: {
+                name: "Twitter NYC Headquarters",
+                address: "245 W 17th St, New York, NY 11238",
+                organizationId,
+            }
+        });
+        expect(createResult.errors).toBe(undefined);
+        expect((_a = createResult.data) === null || _a === void 0 ? void 0 : _a.createLocation.name).toBe("Twitter NYC Headquarters");
+    });
+    it('Fetch Location By Id', async () => {
+        var _a;
+        expect.assertions(2);
+        const findResult = await server.executeOperation({
+            query: LocationQueries.GET_LOCATION,
+            variables: { id: locationId }
+        });
+        expect(findResult.errors).toBe(undefined);
+        expect((_a = findResult.data) === null || _a === void 0 ? void 0 : _a.location.name).toBe("Twitter SF Headquarters");
+    });
+    it('Fetch Locations By Ids', async () => {
+        var _a, _b;
+        expect.assertions(2);
+        const findAllResults = await server.executeOperation({
+            query: LocationQueries.GET_ALL_LOCATIONS
+        });
+        const locationIds = (_a = findAllResults.data) === null || _a === void 0 ? void 0 : _a.allLocations.map((location) => location.id);
+        const results = await server.executeOperation({
+            query: LocationQueries.GET_LOCATIONS,
+            variables: { ids: locationIds }
+        });
+        expect(results.errors).toBe(undefined);
+        expect((_b = results.data) === null || _b === void 0 ? void 0 : _b.locations).toHaveLength(2);
+    });
+    it("Fetch All Locations", async () => {
+        var _a;
+        expect.assertions(2);
+        const findResults = await server.executeOperation({
+            query: LocationQueries.GET_ALL_LOCATIONS
+        });
+        expect(findResults.errors).toBe(undefined);
+        expect((_a = findResults.data) === null || _a === void 0 ? void 0 : _a.allLocations).toHaveLength(2);
+    });
+    it("Updates a Location", async () => {
+        var _a;
+        expect.assertions(2);
+        const updateResult = await server.executeOperation({
+            query: LocationQueries.UPDATE_LOCATION,
+            variables: {
+                name: "Twitter Resort",
+                id: locationId,
+            }
+        });
+        expect(updateResult.errors).toBe(undefined);
+        expect((_a = updateResult.data) === null || _a === void 0 ? void 0 : _a.updateLocation.name).toBe("Twitter Resort");
+    });
+    it("Creates and Deletes a Location", async () => {
+        var _a;
+        expect.assertions(2);
+        const expectedResult = {
+            success: true
+        };
+        const deleteResult = await server.executeOperation({
+            query: LocationQueries.DELETE_LOCATION,
+            variables: { id: locationId }
+        });
+        expect(deleteResult.errors).toBe(undefined);
+        expect((_a = deleteResult.data) === null || _a === void 0 ? void 0 : _a.deleteLocation).toEqual(expectedResult);
+    });
+});
--- build/schema/resolvers/resolvers.test.js
+++ build/schema/resolvers/resolvers.test.js
@@ -18,25 +18,11 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const apollo_server_1 = require("apollo-server");
-const Queries = __importStar(require("../testUtils/Queries"));
-const organization_1 = require("./organization");
-const location_1 = require("./location");
-const event_1 = require("./event");
-const util_1 = require("./util");
-const organization_2 = require("../typeDefs/organization");
-const location_2 = require("../typeDefs/location");
-const event_2 = require("../typeDefs/event");
-const api_1 = __importDefault(require("../../datasources/api"));
-const store_1 = require("../../datasources/store");
-const organization_3 = __importDefault(require("../../datasources/api/organization"));
-const location_3 = __importDefault(require("../../datasources/api/location"));
-const event_3 = __importDefault(require("../../datasources/api/event"));
-const dotenv_1 = require("dotenv");
+const createTestServer_1 = require("../testUtils/createTestServer");
+const OrganizationQueries = __importStar(require("../testUtils/organizationQueries"));
+const LocationQueries = __importStar(require("../testUtils/locationQueries"));
+const EventQueries = __importStar(require("../testUtils/eventQueries"));
 describe("Testing Jest", () => {
     it("Runs a test", async () => {
         const item = 1;
@@ -46,75 +32,64 @@ describe("Testing Jest", () => {
 describe('All Resolvers', () => {
     let server;
     beforeAll(async () => {
-        (0, dotenv_1.config)();
-        const store = (0, store_1.createTestStore)();
-        const organizationAPI = new organization_3.default(store.organization);
-        const locationAPI = new location_3.default(store.location);
-        const eventAPI = new event_3.default(store.event);
-        const dataSources = () => ({
-            api: new api_1.default(organizationAPI, locationAPI, eventAPI)
-        });
-        server = new apollo_server_1.ApolloServer({
-            typeDefs: [organization_2.typeDefs, location_2.typeDefs, event_2.typeDefs],
-            resolvers: [organization_1.resolvers, location_1.resolvers, event_1.resolvers, util_1.resolvers],
-            dataSources
-        });
+        server = await (0, createTestServer_1.createTestServer)();
     });
     afterAll(async () => {
         var _a;
         const result = await server.executeOperation({
-            query: Queries.GET_ALL_ORGANIZATIONS
+            query: OrganizationQueries.GET_ALL_ORGANIZATIONS
         });
         for (const org of (_a = result.data) === null || _a === void 0 ? void 0 : _a.allOrganizations) {
             await server.executeOperation({
-                query: Queries.DELETE_ORGANIZATION,
+                query: OrganizationQueries.DELETE_ORGANIZATION,
                 variables: {
                     id: org.id
                 }
             });
         }
         const result2 = await server.executeOperation({
-            query: Queries.GET_ALL_ORGANIZATIONS
+            query: OrganizationQueries.GET_ALL_ORGANIZATIONS
         });
     });
     it("Create an Organization", async () => {
         var _a;
         expect.assertions(2);
         const result = await server.executeOperation({
-            query: Queries.CREATE_ORGANIZATION,
+            query: OrganizationQueries.CREATE_ORGANIZATION,
             variables: { name: "Polus" }
         });
         expect(result.errors).toBe(undefined);
         expect((_a = result.data) === null || _a === void 0 ? void 0 : _a.createOrganization.name).toBe("Polus");
     });
     it("Creates a Location", async () => {
-        var _a, _b;
-        expect.assertions(3);
+        var _a, _b, _c;
+        expect.assertions(4);
         const findResult = await server.executeOperation({
-            query: Queries.GET_ALL_ORGANIZATIONS
+            query: OrganizationQueries.GET_ALL_ORGANIZATIONS
         });
         const organizationId = (_a = findResult.data) === null || _a === void 0 ? void 0 : _a.allOrganizations[0].id;
         const createResult = await server.executeOperation({
-            query: Queries.CREATE_LOCATION,
+            query: LocationQueries.CREATE_LOCATION,
             variables: {
                 name: "Polus Headquarters",
                 address: "205 W 109th Street, New York, New York 100025",
-                organizationId: organizationId,
+                organizationId,
             }
         });
         expect(findResult.errors).toBe(undefined);
+        expect((_b = findResult.data) === null || _b === void 0 ? void 0 : _b.allOrganizations).toHaveLength(1);
         expect(createResult.errors).toBe(undefined);
-        expect((_b = createResult.data) === null || _b === void 0 ? void 0 : _b.createLocation.name).toBe("Polus Headquarters");
+        expect((_c = createResult.data) === null || _c === void 0 ? void 0 : _c.createLocation.name).toBe("Polus Headquarters");
     });
     it("Creates an Event", async () => {
         var _a, _b;
         expect.assertions(2);
         const findResult = await server.executeOperation({
-            query: Queries.GET_ALL_ORGANIZATIONS
+            query: OrganizationQueries.GET_ALL_ORGANIZATIONS
         });
         const organizationId = (_a = findResult.data) === null || _a === void 0 ? void 0 : _a.allOrganizations[0].id;
         const eventResult = await server.executeOperation({
-            query: Queries.CREATE_EVENT,
+            query: EventQueries.CREATE_EVENT,
             variables: {
                 name: "Party!",
                 dateAndTime: new Date('10/12/2021'),
@@ -128,7 +103,7 @@ describe('All Resolvers', () => {
     it('Fetch Organization by Id', async () => {
         expect.assertions(1);
         const results = await server.executeOperation({
-            query: Queries.GET_ORGANIZATION,
+            query: OrganizationQueries.GET_ORGANIZATION,
             variables: { id: 1 }
         });
         expect(results.errors).toBe(undefined);
@@ -136,7 +111,7 @@ describe('All Resolvers', () => {
     it('Fetch Location By Id', async () => {
         expect.assertions(1);
         const results = await server.executeOperation({
-            query: Queries.GET_LOCATION,
+            query: LocationQueries.GET_LOCATION,
             variables: { id: 1 }
         });
         expect(results.errors).toBe(undefined);
@@ -144,7 +119,7 @@ describe('All Resolvers', () => {
     it('Fetch Event By Id', async () => {
         expect.assertions(1);
         const results = await server.executeOperation({
-            query: Queries.GET_EVENT,
+            query: EventQueries.GET_EVENT,
             variables: { id: 1 }
         });
         expect(results.errors).toBe(undefined);
@@ -153,11 +128,11 @@ describe('All Resolvers', () => {
         var _a;
         expect.assertions(1);
         const findAllResult = await server.executeOperation({
-            query: Queries.GET_ALL_ORGANIZATIONS
+            query: OrganizationQueries.GET_ALL_ORGANIZATIONS
         });
         const organizationIds = (_a = findAllResult.data) === null || _a === void 0 ? void 0 : _a.allOrganizations.map((organization) => organization.id);
         const findResults = await server.executeOperation({
-            query: Queries.GET_ORGANIZATIONS,
+            query: OrganizationQueries.GET_ORGANIZATIONS,
             variables: { id: [organizationIds] }
         });
         expect(findResults.errors).toBe(undefined);
@@ -166,11 +141,11 @@ describe('All Resolvers', () => {
         var _a;
         expect.assertions(1);
         const findAllResult = await server.executeOperation({
-            query: Queries.GET_ALL_LOCATIONS
+            query: LocationQueries.GET_ALL_LOCATIONS
         });
         const locationIds = (_a = findAllResult.data) === null || _a === void 0 ? void 0 : _a.allLocations.map((location) => location.id);
         const findResults = await server.executeOperation({
-            query: Queries.GET_LOCATIONS,
+            query: LocationQueries.GET_LOCATIONS,
             variables: { ids: locationIds }
         });
         expect(findResults.errors).toBe(undefined);
@@ -179,11 +154,11 @@ describe('All Resolvers', () => {
         var _a;
         expect.assertions(1);
         const findAllResult = await server.executeOperation({
-            query: Queries.GET_ALL_EVENTS
+            query: EventQueries.GET_ALL_EVENTS
         });
         const eventIds = (_a = findAllResult.data) === null || _a === void 0 ? void 0 : _a.allEvents.map((event) => event.id);
         const findResults = await server.executeOperation({
-            query: Queries.GET_EVENTS,
+            query: EventQueries.GET_EVENTS,
             variables: { ids: eventIds }
         });
         expect(findResults.errors).toBe(undefined);
@@ -192,7 +167,7 @@ describe('All Resolvers', () => {
         var _a;
         expect.assertions(2);
         const findResults = await server.executeOperation({
-            query: Queries.GET_ALL_LOCATIONS
+            query: LocationQueries.GET_ALL_LOCATIONS
         });
         expect(findResults.errors).toBe(undefined);
         expect((_a = findResults.data) === null || _a === void 0 ? void 0 : _a.allLocations).toHaveLength(1);
@@ -201,7 +176,7 @@ describe('All Resolvers', () => {
         var _a;
         expect.assertions(2);
         const findResults = await server.executeOperation({
-            query: Queries.GET_ALL_EVENTS
+            query: EventQueries.GET_ALL_EVENTS
         });
         expect(findResults.errors).toBe(undefined);
         expect((_a = findResults.data) === null || _a === void 0 ? void 0 : _a.allEvents).toHaveLength(1);
@@ -210,18 +185,18 @@ describe('All Resolvers', () => {
         var _a, _b, _c, _d;
         expect.assertions(4);
         const findResult = await server.executeOperation({
-            query: Queries.GET_ALL_ORGANIZATIONS,
+            query: OrganizationQueries.GET_ALL_ORGANIZATIONS,
         });
         const organizationId = (_a = findResult.data) === null || _a === void 0 ? void 0 : _a.allOrganizations[0].id;
         const updateResult = await server.executeOperation({
-            query: Queries.UPDATE_ORGANIZATION,
+            query: OrganizationQueries.UPDATE_ORGANIZATION,
             variables: {
                 id: organizationId,
                 name: "Pola"
             }
         });
         const revertResult = await server.executeOperation({
-            query: Queries.UPDATE_ORGANIZATION,
+            query: OrganizationQueries.UPDATE_ORGANIZATION,
             variables: {
                 id: organizationId,
                 name: "Polus"
@@ -236,11 +211,11 @@ describe('All Resolvers', () => {
         var _a, _b, _c;
         expect.assertions(4);
         const findResult = await server.executeOperation({
-            query: Queries.GET_ALL_ORGANIZATIONS
+            query: OrganizationQueries.GET_ALL_ORGANIZATIONS
         });
         const locationId = (_a = findResult.data) === null || _a === void 0 ? void 0 : _a.allOrganizations[0].locations[0].id;
         const updateResult = await server.executeOperation({
-            query: Queries.UPDATE_LOCATION,
+            query: LocationQueries.UPDATE_LOCATION,
             variables: {
                 name: "Polus Resort",
                 id: locationId,
@@ -255,11 +230,11 @@ describe('All Resolvers', () => {
         var _a, _b, _c;
         expect.assertions(4);
         const findResult = await server.executeOperation({
-            query: Queries.GET_ALL_ORGANIZATIONS
+            query: OrganizationQueries.GET_ALL_ORGANIZATIONS
         });
         const eventId = (_a = findResult.data) === null || _a === void 0 ? void 0 : _a.allOrganizations[0].events[0].id;
         const updateResult = await server.executeOperation({
-            query: Queries.UPDATE_EVENT,
+            query: EventQueries.UPDATE_EVENT,
             variables: {
                 id: eventId,
                 name: "Birthday",
@@ -276,7 +251,7 @@ describe('All Resolvers', () => {
         var _a, _b, _c;
         expect.assertions(4);
         const createResult = await server.executeOperation({
-            query: Queries.CREATE_ORGANIZATION,
+            query: OrganizationQueries.CREATE_ORGANIZATION,
             variables: { name: "Google" }
         });
         const expectedResult = {
@@ -284,7 +259,7 @@ describe('All Resolvers', () => {
         };
         const organizationId = (_a = createResult.data) === null || _a === void 0 ? void 0 : _a.createOrganization.id;
         const deleteResult = await server.executeOperation({
-            query: Queries.DELETE_ORGANIZATION,
+            query: OrganizationQueries.DELETE_ORGANIZATION,
             variables: { id: organizationId }
         });
         expect(createResult.errors).toBe(undefined);
@@ -299,11 +274,11 @@ describe('All Resolvers', () => {
             success: true
         };
         const findResult = await server.executeOperation({
-            query: Queries.GET_ALL_ORGANIZATIONS
+            query: OrganizationQueries.GET_ALL_ORGANIZATIONS
         });
         const organizationId = (_a = findResult.data) === null || _a === void 0 ? void 0 : _a.allOrganizations[0].id;
         const createResult = await server.executeOperation({
-            query: Queries.CREATE_LOCATION,
+            query: LocationQueries.CREATE_LOCATION,
             variables: {
                 name: "Company Island",
                 address: "23 Happy Valley Rd, Pembroke, Bermuda",
@@ -312,7 +287,7 @@ describe('All Resolvers', () => {
         });
         const locationId = (_b = createResult.data) === null || _b === void 0 ? void 0 : _b.createLocation.id;
         const deleteResult = await server.executeOperation({
-            query: Queries.DELETE_LOCATION,
+            query: LocationQueries.DELETE_LOCATION,
             variables: { id: locationId }
         });
         expect(findResult.errors).toBe(undefined);
@@ -326,11 +301,11 @@ describe('All Resolvers', () => {
             success: true
         };
         const findResult = await server.executeOperation({
-            query: Queries.GET_ALL_ORGANIZATIONS
+            query: OrganizationQueries.GET_ALL_ORGANIZATIONS
         });
         const organizationId = (_a = findResult.data) === null || _a === void 0 ? void 0 : _a.allOrganizations[0].id;
         const createResult = await server.executeOperation({
-            query: Queries.CREATE_EVENT,
+            query: EventQueries.CREATE_EVENT,
             variables: {
                 name: "Stockholder Meeting",
                 dateAndTime: new Date('11/11/2021'),
@@ -340,7 +315,7 @@ describe('All Resolvers', () => {
         });
         const eventId = (_b = createResult.data) === null || _b === void 0 ? void 0 : _b.createEvent.id;
         const deleteResult = await server.executeOperation({
-            query: Queries.DELETE_EVENT,
+            query: EventQueries.DELETE_EVENT,
             variables: { id: eventId }
         });
         expect(findResult.errors).toBe(undefined);
